1945bcdd8a15b492e7a79d9da450d8f933681b30
hyperf1x <muhjh6@gmail.com>
2020-04-26 13:07:59 +0900

    fix

---
+++ gara-mmsa/getDate.js
@@ -0,0 +1,10 @@
+var today = new Date();  
+var dateString = "2020-01-12";  
+  
+var dateArray = dateString.split("-");  
+  
+var dateObj = new Date(dateArray[0], Number(dateArray[1])-1, dateArray[2]);  
+  
+var betweenDay = (today.getTime() - dateObj.getTime())/1000/60/60/24;  
+  
+document.write(Math.floor(betweenDay));
