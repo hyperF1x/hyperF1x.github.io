610dc7bd76e60d9cd03cdf27d95ab835d8ecb807
hyperF1x <muhjh6@gmail.com>
2020-05-05 18:49:56 +0900

    change return to innerHTML

--- hoaxmmsa/getDate.js
+++ hoaxmmsa/getDate.js
@@ -7,13 +7,29 @@ function getDate(dateString) {
 
     var betweenDay = (today.getTime() - dateObj.getTime())/1000/60/60/24;
 
-    return Math.floor(betweenDay);
+    document.getElementById("pastDay").innerHTML = Math.floor(betweenDay) + "일";
 }
 
-function getTime(hour, minutes) {         
+function getTime(hour, minutes) {
+    let old = new Date(00, 0, 0, hour, minutes);
+    old = old.getTime();
+    let now = new Date().getTime();
+
+    let date = new Date(now - old);
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    if(hh < 10)
+        hh = "0" + hh;
+    if(mm < 10)
+        mm = "0" + mm;
+    if(ss < 10)
+        ss = "0" + ss;
+    
+    document.getElementById("time").innerHTML = hh + ":" + mm + ":" + ss;
+
     timer = setInterval(() => {
-        let old = new Date(00, 0, 0, hour, minutes);
-        old = old.getTime();
         let now = new Date().getTime();
 
         let date = new Date(now - old);
